2048bf8193224a75022b98b7676b27debdfb76d3
Josceline F <113638071+JosFen@users.noreply.github.com>
2025-01-25 12:19:25 -0800

    added events panel

--- src/components/EventCalendar.tsx
+++ src/components/EventCalendar.tsx
@@ -1,6 +1,9 @@
 "use client"
+import eventsData  from "@/lib/eventsData";
 import { useState } from "react";
 import Calendar from "react-calendar";
+import "react-calendar/dist/Calendar.css";
+import Image from "next/image";
 
 type ValuePiece = Date | null;
 type Value = ValuePiece | [ValuePiece, ValuePiece];
@@ -9,8 +12,25 @@ const EventCalendar = () => {
     const [value, onChange] = useState<Value>(new Date());
 
     return (
-        <div>
+        <div className="bg-white p-3 rounded-xl">
             <Calendar onChange={onChange} value={value} /> 
+            <div className="flex items-cener justify-between"> 
+                <h2 className="text-lg font-semibold my-4">Events</h2>
+                <div className="flex items-center gap-4">
+                <Image src="/moreDark.png" alt="more" width={20} height={20} />
+                </div>
+            </div>
+            <div className="flex flex-col gap-1" >
+                {eventsData.map((event) => (
+                    <div key={event.id} className="p-2 rounded-lg border-t-2 odd:border-t-blue-100 even:border-t-pink-100">
+                        {/* <div className="flex items-center justify-between"> */}
+                        <h3 className="font-bold text-gray-600">{event.title}</h3>
+                        <p className="text-gray-400 text-xs">{event.time}</p>
+                        {/* </div> */}
+                        <p className="mt-1 text-gray-500 text-sm">{event.description}</p>
+                    </div>
+                ))}
+            </div>
         </div>
     );
 };
---
+++ src/lib/eventsData.ts
@@ -0,0 +1,37 @@
+type ValuePiece = Date | null;
+type Value = ValuePiece | [ValuePiece, ValuePiece];
+
+const eventsData = [
+    {
+        id: 1,
+        title: "Fun Event",
+        time: "10:00 AM - 11:00 AM",
+        description: "Get ready for the first event",
+    },
+    {
+        id: 2,
+        title: "Dance Event",
+        time: "11:00 AM - 12:00 PM",
+        description: "Dance dance dance",
+    },
+    {
+        id: 3,
+        title: "Music Event",
+        time: "12:00 PM - 1:00 PM",
+        description: "Listen to musi, unwind",
+    },
+    {
+        id: 4,
+        title: "Game Event",
+        time: "1:00 PM - 2:00 PM",
+        description: "Game on, video games, board games",
+    },
+    {
+        id: 5,
+        title: "Food Event",
+        time: "2:00 PM - 3:00 PM",
+        description: "Food day, eat and be merry",
+    }
+];
+
+export default eventsData;

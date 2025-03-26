470a3f440cfd41da7d8ceeb229fd92dd9d76ad5a
Josceline F <113638071+JosFen@users.noreply.github.com>
2025-03-25 23:08:40 -0700

    added prisma schema

--- src/components/FormModal.tsx
+++ src/components/FormModal.tsx
@@ -10,7 +10,6 @@ import {
 import dynamic from 'next/dynamic'
 import Image from 'next/image'
 import { useState } from 'react'
-// import TeacherForm from './forms/TeacherForm'
 
 // USE LAZY LOADING
 
